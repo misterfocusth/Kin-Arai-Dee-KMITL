0176a09fd2b76c388bd7a0b52cf8af2abc145ee9
BNKT <65070021@kmitl.ac.th>
2022-11-17 15:56:23 +0700

    Update README.md and Models (__repr__)

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import func
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
 
 
@@ -19,12 +18,15 @@ class User(db.Model):
 
 class Restaurant_Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    restaurant_id = db.Column(db.Integer, primary_key=True)
+    restaurant_id = db.Column(db.Integer)
     review_by = db.Column(db.String(255), nullable=False)
     review_content = db.Column(db.String(255), nullable=False)
     review_rating = db.Column(db.Integer, nullable=False)
     review_date = db.Column(db.DateTime(timezone=True), default=func.now())
     is_deleted = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f"Restaurant_Review(restaurant_id={self.restaurant_id}, review_by={self.review_by}, review_content={self.review_content}, review_rating={self.review_rating}, review_date={self.review_date}, is_deleted={self.is_deleted})"
 
 
 class Restaurant(db.Model):
@@ -34,22 +36,31 @@ class Restaurant(db.Model):
     phone_number = db.Column(db.String(255), nullable=False)
     category = db.Column(db.String(255), nullable=False)
 
+    def __repr__(self):
+        return f"Restaurant(name={self.name}, location={self.location}, phone_number={self.phone_number}, category={self.category})"
+
 
 class Menu_Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    menu_id = db.Column(db.Integer, primary_key=True)
+    menu_id = db.Column(db.Integer, nullable=False)
     review_by = db.Column(db.String(255), nullable=False)
     review_content = db.Column(db.String(255), nullable=False)
     review_rating = db.Column(db.Integer, nullable=False)
     review_date = db.Column(db.DateTime(timezone=True), default=func.now())
     is_deleted = db.Column(db.Boolean, default=False)
 
+    def __repr__(self):
+        return f"Menu_Review(menu_id={self.menu_id}, review_by={self.review_by}, review_content={self.review_content}, review_rating={self.review_rating}, review_date={self.review_date}, is_deleted={self.is_deleted})"
+
 
 class Menu(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
     price = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(255), nullable=False)
     restaurant_name = db.Column(db.String(255), nullable=False)
     restaurant_location = db.Column(db.String(255), nullable=False)
     image_url = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f"Menu(name={self.name}, price={self.price}, category={self.category}, restaurant_name={self.restaurant_name}, restaurant_location={self.restaurant_location}, image_url={self.image_url})"
--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,5 @@ from app import create_app
 
 app = create_app()
 
-
 if __name__ == "__main__":
     app.run(debug=True)
