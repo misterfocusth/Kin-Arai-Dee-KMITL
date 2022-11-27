7658ec5ee472b70c89411a8f184a7a75afcd3b5a
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2022-11-27 23:34:22 +0700

    Delete | Backup project files folder

--- backup/admin/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
--- backup/frontend/src/components/MyLoader.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from "@mantine/core";
-
-export default function MyLoader() {
-  return (
-    <div className="flex min-h-screen min-w-screen justify-center items-center">
-      <Loader color="yellow" size="xl" variant="dots" />
-    </div>
-  );
-}
